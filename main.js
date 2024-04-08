7e1b9d3396faa1fae4c3a000f6f7e5bffa8cb0ec
Danny2727 <dannyedouard14@gmail.com>
2024-04-08 03:31:45 -0400

    created a diplay todos function that updates todo list and addds functionality to the buttons

--- main.js
+++ main.js
@@ -26,8 +26,81 @@ window.addEventListener('load', () => {
         localStorage.setItem('todos', JSON.stringify(todos));
 
         e.target.reset();
+
+
+        DisplayTodos();
+
+
     })
 })
+
+function DisplayTodos() {
+    const todoList = document.getElementById('#todo-list');
+    todoList.innerHTML = '';
+
+    todos.forEach((todo) => {
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item');
+
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteBtn = document.createElement('button');
+
+        input.type = 'checkbox';
+        input.checked = todo.done;
+        span.classList.add('bublle');
+
+        if (todo.category === "personal") {
+            span.classList.add('personal');
+        } else{
+            span.classList.add('business');
+        }
+
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit'); 
+        deleteBtn.classList.add('delete'); 
+
+
+        content.innerHTML = `input type="text" value="${todo.content}" readonly`;
+        edit.innerHTML = 'Edit';
+        deleteBtn.innerHTML = 'Delete';
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteBtn);
+        todoItem.appendChild(label);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
+
+        todoList.appendChild(todoItem);
+
+        if(todo.done){
+            todoItem.classList.add('done');
+        }
+
+        input.addEventListener('click', e => {
+            todo.done = e.target.checked;
+            localStorage.setItem('todos', JSON.stringify(todos));
+            DisplayTodos();
+
+            if(todo.done){
+                todoItem.classList.add('done');
+            } else {
+                todoItem.classList.remove('done');
+            }
+
+
+            DisplayTodos
+
+    
+    });
+}
 
 
 
