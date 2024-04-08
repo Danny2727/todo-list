05bf25991f0e7a13a41b0dd599b784762feba7f6
Danny2727 <dannyedouard14@gmail.com>
2024-04-08 03:18:47 -0400

    finsihed the local storage code in main.js that saves username, todos, and which category it belongs to

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
 window.addEventListener('load', () => {
     todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const nameInput = document.getElementById('#name');
+    const newTodoForm = document.getElementById('#new-todo-form');
+
+    const username = localStorage.getItem('username');
+
+    nameInput.value = username;
+
+    nameInput.addEventListener('input', e => {
+        localStorage.setItem('username', e.target.value);
+    });
+
+    newTodoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const todo = {
+            content: e.target.elements['content'].value,
+            category: e.target.elements['category'].value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
+
+        todos.push(todo);
+
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+        e.target.reset();
+    })
 })
+
+
 
